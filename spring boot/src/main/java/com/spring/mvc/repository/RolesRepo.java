438ed0ed1d1080fc73b919987f5a2b7c19a87b7e
Yusvanth-Subramanian <136310592+Yusvanth-Subramanian@users.noreply.github.com>
2023-07-07 18:35:58 +0530

    Delete spring boot directory

--- spring boot/src/main/java/com/spring/mvc/repository/RolesRepo.java
+++
@@ -1,9 +0,0 @@
-package com.spring.mvc.repository;
-
-import com.spring.mvc.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolesRepo extends JpaRepository<Role,Integer> {
-}
